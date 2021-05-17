70a26d01e928ef9af85be50d160b3b12d3d4ee07
Cody Doucette <doucette@bu.edu>
2021-05-17 16:43:58 -0400

    Netlink: add userspace NL port option for KRT

--- sysdep/linux/netlink.c
+++ sysdep/linux/netlink.c
@@ -145,6 +145,8 @@ static linpool *nl_linpool;
 static struct nl_sock nl_scan = {.fd = -1};	/* Netlink socket for synchronous scan */
 static struct nl_sock nl_req  = {.fd = -1};	/* Netlink socket for requests */
 
+static u32 krt_nl_pid; /* Netlink port ID for KRT requests and scans */
+
 static void
 nl_open_sock(struct nl_sock *nl)
 {
@@ -1407,7 +1409,7 @@ dest:
     }
 
   /* Ignore missing for DELETE */
-  return nl_exchange(&r->h, (op == NL_OP_DELETE), NL_PID_KERNEL);
+  return nl_exchange(&r->h, (op == NL_OP_DELETE), krt_nl_pid);
 }
 
 static inline int
@@ -1915,7 +1917,7 @@ krt_do_scan(struct krt_proto *p UNUSED)	/* CONFIG_ALL_TABLES_AT_ONCE => p is NUL
   struct nl_parse_state s;
 
   nl_parse_begin(&s, 1);
-  nl_request_dump(AF_UNSPEC, RTM_GETROUTE, NL_PID_KERNEL);
+  nl_request_dump(AF_UNSPEC, RTM_GETROUTE, krt_nl_pid);
   while (h = nl_get_scan())
     if (h->nlmsg_type == RTM_NEWROUTE || h->nlmsg_type == RTM_DELROUTE)
       nl_parse_route(&s, h);
@@ -2090,6 +2092,8 @@ krt_sys_start(struct krt_proto *p)
 
   HASH_INSERT2(nl_table_map, RTH, krt_pool, p);
 
+  krt_nl_pid = KRT_CF->sys.nl_pid;
+
   nl_open();
   nl_open_async();
 
@@ -2105,7 +2109,9 @@ krt_sys_shutdown(struct krt_proto *p)
 int
 krt_sys_reconfigure(struct krt_proto *p UNUSED, struct krt_config *n, struct krt_config *o)
 {
-  return (n->sys.table_id == o->sys.table_id) && (n->sys.metric == o->sys.metric);
+  return (n->sys.table_id == o->sys.table_id) &&
+    (n->sys.metric == o->sys.metric) &&
+    (n->sys.nl_pid == o->sys.nl_pid);
 }
 
 void
@@ -2113,6 +2119,8 @@ krt_sys_init_config(struct krt_config *cf)
 {
   cf->sys.table_id = RT_TABLE_MAIN;
   cf->sys.metric = 32;
+  /* Scan and send updates to kernel by default. */
+  cf->sys.nl_pid = NL_PID_KERNEL;
 }
 
 void
@@ -2120,6 +2128,7 @@ krt_sys_copy_config(struct krt_config *d, struct krt_config *s)
 {
   d->sys.table_id = s->sys.table_id;
   d->sys.metric = s->sys.metric;
+  d->sys.nl_pid = s->sys.nl_pid;
 }
 
 static const char *krt_metrics_names[KRT_METRICS_MAX] = {
